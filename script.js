427a0f880852c584290be4f0551f481ae34f653d
wendel m <wendelmjesus@outlook.com>
2026-06-01 23:50:56 -0300

    add interactive homepage banner carousel.

--- script.js
+++ script.js
@@ -16,3 +16,65 @@ const accountBtn = document.querySelector(".account-btn");
 accountBtn.addEventListener("click", () => {
     account.classList.toggle("active");
 });
+
+const bannerImg = document.querySelector("#banner-img");
+const bannerBtn = document.querySelector(".banner-btn");
+const leftArrow = document.querySelector(".banner-arrow-left");
+const rightArrow = document.querySelector(".banner-arrow-right");
+
+const banners = [
+    {
+        image: "assets/images/banner-1.png",
+        alt: "Banner de Brinquedos",
+        buttonText: "Confira nossos brinquedos"
+    },
+    {
+        image: "assets/images/banner-2.png",
+        alt: "Banner de Rações",
+        buttonText: "Veja nossas rações"
+    }
+];
+
+let currentBanner = 0;
+let isChangingBanner = false;
+
+function updateBanner(nextBanner) {
+    if (isChangingBanner) {
+        return;
+    }
+
+    isChangingBanner = true;
+    bannerImg.classList.add("is-changing");
+
+    setTimeout(() => {
+        currentBanner = nextBanner;
+        bannerImg.src = banners[currentBanner].image;
+        bannerImg.alt = banners[currentBanner].alt;
+        bannerBtn.textContent = banners[currentBanner].buttonText;
+        bannerImg.classList.remove("is-changing");
+
+        setTimeout(() => {
+            isChangingBanner = false;
+        }, 350);
+    }, 350);
+}
+
+rightArrow.addEventListener("click", () => {
+    let nextBanner = currentBanner + 1;
+
+    if (nextBanner >= banners.length) {
+        nextBanner = 0;
+    }
+
+    updateBanner(nextBanner);
+});
+
+leftArrow.addEventListener("click", () => {
+    let nextBanner = currentBanner - 1;
+
+    if (nextBanner < 0) {
+        nextBanner = banners.length - 1;
+    }
+
+    updateBanner(nextBanner);
+});
